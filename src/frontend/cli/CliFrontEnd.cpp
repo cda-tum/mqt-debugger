d2ce68a91f18be30b47d18963dc28f57de580c86
DRovara <damian.rovara@tum.de>
2024-08-20 12:46:58 +0200

    feat: :sparkles: CliFrontEnd now works with up to 5 qubits. If there are more qubits, the state vector is no longer shown

--- src/frontend/cli/CliFrontEnd.cpp
+++ src/frontend/cli/CliFrontEnd.cpp
@@ -73,7 +73,7 @@ void CliFrontEnd::run(const char* code, SimulationState* state) {
       }
       wasGet = false;
     }
-    printState(state, inspecting, state->getNumQubits(state) != 3);
+    printState(state, inspecting, state->getNumQubits(state) >= 6);
 
     std::cout << "Enter command: ";
     std::getline(std::cin, command);
@@ -104,6 +104,18 @@ void CliFrontEnd::run(const char* code, SimulationState* state) {
       wasError = true;
     }
   }
+}
+
+std::vector<std::string> getBitStrings(size_t numQubits) {
+  std::vector<std::string> bitStrings;
+  for (size_t i = 0; i < (1ULL << numQubits); i++) {
+    std::string bitString;
+    for (size_t j = 0; j < numQubits; j++) {
+      bitString.insert(bitString.begin(), (i & (1 << j)) > 0 ? '1' : '0');
+    }
+    bitStrings.push_back(bitString);
+  }
+  return bitStrings;
 }
 
 void CliFrontEnd::printState(SimulationState* state, size_t inspecting,
@@ -166,11 +178,10 @@ void CliFrontEnd::printState(SimulationState* state, size_t inspecting,
   std::cout << "\n";
 
   if (!codeOnly) {
-    const std::array<const char*, 8> bitStrings = {"000", "001", "010", "011",
-                                                   "100", "101", "110", "111"};
+    const auto bitStrings = getBitStrings(state->getNumQubits(state));
     Complex c;
-    for (const auto* bitString : bitStrings) {
-      state->getAmplitudeBitstring(state, bitString, &c);
+    for (const auto& bitString : bitStrings) {
+      state->getAmplitudeBitstring(state, bitString.c_str(), &c);
       std::cout << bitString << " " << c.real << "\t||\t";
     }
     std::cout << "\n";
