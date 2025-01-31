1d402d06c97b4e46900e0ff2d1e7e0200b210358
Damian Rovara <damianrovara@gmail.com>
2025-01-31 17:28:16 +0100

    refactor(assertion-compilation): :recycle: set up __init__.py for `check` module correctly

--- src/mqt/debugger/check/__init__.py
+++ src/mqt/debugger/check/__init__.py
@@ -2,9 +2,13 @@
 
 from __future__ import annotations
 
+from .calibration import Calibration
 from .result_checker import Result, check_result
+from .run_preparation import start_compilation
 
 __all__ = [
+    "Calibration",
     "Result",
     "check_result",
+    "start_compilation",
 ]
