8444952630722c2d296e474bc00a0f8123db7431
Damian Rovara <damianrovara@gmail.com>
2025-02-18 12:15:44 +0100

    feat(assertion-compilation): :sparkles: add better success probability estimation to calibration

--- src/mqt/debugger/check/calibration.py
+++ src/mqt/debugger/check/calibration.py
@@ -4,15 +4,21 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
+from qiskit import QuantumCircuit
+
 
 @dataclass
 class Calibration:
     """Represents the calibration data for a quantum device."""
 
-    error_rate: float
+    error_rate_1q: float
+    error_rate_2q: float
+    error_rate_measurement: float
 
     def get_expected_success_probability(self, code: str) -> float:
         """Get the expected success probability of the device for some program.
+
+        Currently, expected success probability is computed in terms of measurement and gate fidelities.
 
         Args:
             code (str): The program to check.
@@ -20,10 +26,27 @@ class Calibration:
         Returns:
             float: The expected success probability.
         """
-        instructions = code.count(";")
-        return (1 - self.error_rate) ** instructions
+        fidelity_measurement = 1 - self.error_rate_measurement
+        fidelity_1q = 1 - self.error_rate_1q
+        fidelity_2q = 1 - self.error_rate_2q
+
+        gate_fidelity = 1.0
+        qc = QuantumCircuit.from_qasm_str(code)
+        for instruction in qc.data:
+            gate_type = instruction.name
+            if gate_type == "barrier":
+                continue
+            if len(instruction.qubits) == 1:
+                if gate_type == "measure":
+                    gate_fidelity *= fidelity_measurement
+                else:
+                    gate_fidelity *= fidelity_1q
+            else:
+                gate_fidelity *= fidelity_2q
+
+        return gate_fidelity
 
     @classmethod
     def example(cls) -> Calibration:
         """Get an example calibration."""
-        return cls(0.01)
+        return cls(0.01, 0.01, 0.01)
